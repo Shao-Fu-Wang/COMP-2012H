06e2c990e8b66f044fba687c189c50d57b43867f
Shao-Fu-Wang <michaelwang920324@gmail.com>
2022-12-21 16:48:50 +0800

    CRY

--- tester.cpp
+++ tester.cpp
@@ -1,14 +1,10 @@
-#include <bits/stdc++.h>
-
+#include <iostream> /* File: fcn-template-2arg-rbv.cpp */
 using namespace std;
-
-
-int * c_i(int value){
-    int x = value;
-    int *p = &x;
-    return p;
-}
-int main(){
-    int *ip = c_i(10);
-    cout << *ip << endl;
+template <typename T1, typename T2> inline T1
+larger(const T1& a, const T2& b) { return (a < b) ? b : a; }
+int main()
+{
+cout << larger(4, 5.5) << endl; // T1 is int, T2 is double
+cout << larger(5.5, 4) << endl; // T1 is double, T2 is int
+return 0;
 }
